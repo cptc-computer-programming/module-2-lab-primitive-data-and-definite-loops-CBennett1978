f24b4208f57e7e6a67fd282d1ae89062a87f9a40
CBennett1978 <CedricB1978@icloud.com>
2026-01-15 10:53:32 -0800

    implemented closing countdown
    implemented closing countdown

--- day2/part01_receipt_totals/ReceiptTotals.java
+++ day2/part01_receipt_totals/ReceiptTotals.java
@@ -16,13 +16,32 @@ public class ReceiptTotals {
     // - Calculate and print the total cost.
     // - Do NOT hard-code the total.
     public static void printReceiptSummary() {
+        int totalCost = 0;
+    } {
+        //loop through items 100 to 105
+        for (int item=100; item ++) {
+            if (item = 100)
+        } {
+            //take a running total
+            //where every item is $5
+            totalCost += 5;
+            //except every 3rd item is $3
+            if (item % 3 == 0) {
+                totalCost -= 2; //reduce $2 from $5 to make it $3
+            }
 
+        
+        })
     }
 
     // TODO:
     // Print a countdown from 5 down to 0 on one line.
     public static void printClosingCountdown() {
-    
+    printClosingCountdown();
+    for (int i=5; i>=0; i --) {
+        System.out.print(i + "");   
+    }
     }
     
 }
+
