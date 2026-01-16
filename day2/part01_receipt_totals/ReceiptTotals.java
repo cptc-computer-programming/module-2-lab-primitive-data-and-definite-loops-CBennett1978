5bca9e22673602f9c534afb0fd43c18e56f0b209
CBennett1978 <CedricB1978@icloud.com>
2026-01-16 03:13:45 -0800

    Update ReceiptTotals.java
    adjusting code

--- day2/part01_receipt_totals/ReceiptTotals.java
+++ day2/part01_receipt_totals/ReceiptTotals.java
@@ -7,41 +7,42 @@ public class ReceiptTotals {
         printClosingCountdown();
     }
 
-    // Scenario:
-    // A store sold items numbered 100 through 105.
-    // Each item costs $5.
-    // Every 3rd item starting at 100 is on sale for $3 instead.
-    //
-    // TODO:
-    // - Calculate and print the total cost.
-    // - Do NOT hard-code the total.
-    public static void printReceiptSummary() {
-        int totalCost = 0;
-    } {
-        //loop through items 100 to 105
-        for (int item=100; item ++) {
-            if (item = 100)
-        } {
-            //take a running total
-            //where every item is $5
-            totalCost += 5;
-            //except every 3rd item is $3
-            if (item % 3 == 0) {
-                totalCost -= 2; //reduce $2 from $5 to make it $3
-            }
+    /* 
+    Scenario:
+        A store sold items numbered 100 through 105
+        Each item costs $5.
+        Every 3rd item starting at 100 is on sale for $3 instead
+    */
 
-        
-        })
-    }
-
-    // TODO:
-    // Print a countdown from 5 down to 0 on one line.
-    public static void printClosingCountdown() {
-    printClosingCountdown();
-    for (int i=5; i>=0; i --) {
-        System.out.print(i + "");   
-    }
-    }
+    /*  
+    TO-DO:
+        - Calculate and print the total cost
+        - Do NOT hard-code the total
+    */
+public static void printReceiptSummary() {
+    //define variable outside of the loop
+    int totalCost = 0;
     
-}
+    for (int item = 100; item <= 105; item++) {
+        // the modulo operator (%) will give the leftover after integer division 
+        if ((item - 100) % 3 == 0) {
+        System.out.println("item number at discount: " + item);
+            totalCost += 3;
+        } else {
+            // take a running total
+            // where every item is $5
+            totalCost += 5;
+        }
+    }
+    System.out.println("Total Cost: $" + totalCost);
+    }
+    // TO-DO:
+        // Print a countdown from 5 down to 0 on one line.
+    public static void printClosingCountdown() {
+        for (int i = 5; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println(); 
+       }
+    }
 
