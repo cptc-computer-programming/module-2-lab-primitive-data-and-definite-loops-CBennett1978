8c6a1ab03ddc43718c8712b157a0eb5d049f0861
CBennett1978 <CedricB1978@icloud.com>
2026-01-20 07:20:30 -0800

    print receipt

--- day2/part01_receipt_totals/ReceiptTotals.java
+++ day2/part01_receipt_totals/ReceiptTotals.java
@@ -1,22 +1,20 @@
 package day2.part01_receipt_totals;
 
 public class ReceiptTotals {
+
     public static void main(String[] args) {
         printReceiptSummary();
         printClosingCountdown();
     }
-    /* 
-    Scenario:
-        A store sold items numbered 100 through 105
-        Each item costs $5.
-        Every 3rd item starting at 100 is on sale for $3 instead
-    */
 
-    /*  
-    TO-DO:
-        - Calculate and print the total cost
-        - Do NOT hard-code the total
-    */
+    // Scenario:
+    // A store sold items numbered 100 through 105.
+    // Each item costs $5.
+    // Every 3rd item starting at 100 is on sale for $3 instead.
+    
+    // TO-DO:
+    // - Calculate and print the total cost
+    // - Do NOT hard-code the total
 public static void printReceiptSummary() {
     //define variable outside of the loop
     int totalCost = 0;
@@ -32,10 +30,11 @@ public static void printReceiptSummary() {
             totalCost += 5;
         }
     }
+    
     System.out.println("Total Cost: $" + totalCost);
     }
     // TO-DO:
-        // Print a countdown from 5 down to 0 on one line.
+    // Print a countdown from 5 down to 0 on one line.
     public static void printClosingCountdown() {
         for (int i = 5; i >= 0; i--) {
             System.out.print(i + " ");
@@ -43,4 +42,3 @@ public static void printReceiptSummary() {
         System.out.println(); 
        }
     }
-
